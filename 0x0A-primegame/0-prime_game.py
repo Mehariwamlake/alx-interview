1e87a40b4d1887afa5b644cf6868437e33c62188
Your Name <you@example.com>
2023-03-07 10:18:43 +0300

    last

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
 
-"""
-prime game
-"""
-
 def isWinner(x, nums):
+    '''
+    Returns the name of the winner
+    '''
     winner = []
     for num in nums:
         winner.append(roundWinner(x, num))
     if winner.count('Ben') > (len(winner)/2):
         return 'Ben'
-    return "Maria"
-    """
-    cakcuates who win
-    """
+    return 'Maria'
 
 
 def roundWinner(x, num):
+    '''
+    Calculates who wons every round
+    '''
     if num == 1:
         return 'Ben'
-    if nums == 2:
+    if num == 2:
         return 'Maria'
-    prime_num = 1
+    prime_numbers = 1
     for n in range(3, num + 1):
         i = 1
         not_prime = 0
-        while (n - 1) >= 2:
-            if n% (n -i) == 0:
+        while (n - i) >= 2:
+            if n % (n - i) == 0:
                 not_prime += 1
             i += 1
         if not_prime == 0:
-            prime_num += 1
-    if prime_num % 2 == 0:
+            prime_numbers += 1
+    if prime_numbers % 2 == 0:
         return 'Ben'
     return 'Maria'
